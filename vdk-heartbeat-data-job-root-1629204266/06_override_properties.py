aceeb2c81f527e20b70a183759bc35f261a42696
aivanov <>
2021-08-17 12:44:37 +0000

    Update data job: vdk-heartbeat-data-job-root-1629204266
    Updating heartbeat data job
    
    Signed-off-by: aivanov

---
+++ vdk-heartbeat-data-job-root-1629204266/06_override_properties.py
@@ -0,0 +1,10 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "memory.default"
+    props['table_source'] = "vdk_heartbeat_source_root_1629204266"
+    props['table_destination'] = "vdk_heartbeat_destination_root_1629204266"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_root_1629204266"
+    props['job_name'] = "vdk-heartbeat-data-job-root-1629204266"
+    job_input.set_all_properties(props)
+        
