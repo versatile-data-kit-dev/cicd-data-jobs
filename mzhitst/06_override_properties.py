50d3a59f372054d0f54100149064095d2c59f0ba
mzhivkov <>
2021-08-23 15:20:54 +0000

    Update data job: mzhitst
    Updating heartbeat data job
    
    Signed-off-by: mzhivkov

---
+++ mzhitst/06_override_properties.py
@@ -0,0 +1,10 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "taurus_testing_sandbox"
+    props['table_source'] = "vdk_heartbeat_source_mzhivkov_1629732049"
+    props['table_destination'] = "vdk_heartbeat_destination_mzhivkov_1629732049"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_mzhivkov_1629732049"
+    props['job_name'] = "mzhitst"
+    job_input.set_all_properties(props)
+        
