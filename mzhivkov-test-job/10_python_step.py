8456ce0a693a01e1e3ee5c96361df1000eeb43f3
mzhivkov <>
2021-09-08 13:43:08 +0000

    Update data job: mzhivkov-test-job
    testing
    
    Signed-off-by: mzhivkov

---
+++ mzhivkov-test-job/10_python_step.py
@@ -0,0 +1,20 @@
+import logging
+import time
+
+log = logging.getLogger(__name__)
+
+
+def run(job_input):
+    """
+    Function named `run` is required in order for a python script to be recognized as a Data Job Python step and executed.
+
+    VDK provides to every python step an object - job_input - that has methods for:
+
+    * executing queries to OLAP Database.
+    * ingesting data into Data Lake
+    * processing Data Lake data into a dimensional model Data Warehouse.
+    See IJobInput documentation
+    """
+    log.info("*******************Sleeping for 180 seconds*******************")
+    time.sleep(180)
+    log.info(f"***************Hello WORLD *****************")
