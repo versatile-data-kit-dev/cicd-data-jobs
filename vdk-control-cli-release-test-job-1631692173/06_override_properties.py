94e884366430fa5f368c882ac0cf490bf601b372
aivanov <>
2021-09-15 07:49:37 +0000

    Update data job: vdk-control-cli-release-test-job-1631692173
    Updating heartbeat data job
    
    Signed-off-by: aivanov

---
+++ vdk-control-cli-release-test-job-1631692173/06_override_properties.py
@@ -0,0 +1,10 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "taurus_testing_sandbox"
+    props['table_source'] = "vdk_heartbeat_source_root_1631692173"
+    props['table_destination'] = "vdk_heartbeat_destination_root_1631692173"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_root_1631692173"
+    props['job_name'] = "vdk-control-cli-release-test-job-1631692173"
+    job_input.set_all_properties(props)
+        
