8c1f54ff34181a90dafdaeb51d1ecbeb18901081
aivanov <>
2021-09-16 10:10:54 +0000

    Update data job: vdk-heartbeat-data-job-gageorgiev-1631787047
    Updating heartbeat data job
    
    Signed-off-by: aivanov

---
+++ vdk-heartbeat-data-job-gageorgiev-1631787047/06_override_properties.py
@@ -0,0 +1,10 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "taurus_testing_sandbox"
+    props['table_source'] = "vdk_heartbeat_source_gageorgiev_1631787047"
+    props['table_destination'] = "vdk_heartbeat_destination_gageorgiev_1631787047"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_gageorgiev_1631787047"
+    props['job_name'] = "vdk-heartbeat-data-job-gageorgiev-1631787047"
+    job_input.set_all_properties(props)
+        
