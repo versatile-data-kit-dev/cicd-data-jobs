2175a81db8e5978e131ec6e3310ffda73fd2db9b
root <root@cicd-control-service-dep-6f59b76588-57n4l>
2024-02-16 10:56:35 +0000

    Updating heartbeat data job

---
+++ vdk-heartbeat-release-test-job-1708080989/06_override_properties.py
@@ -0,0 +1,15 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "taurus_testing_sandbox"
+    props['table_source'] = "vdk_heartbeat_source_vdk_heartbeat_1179097076"
+    props['table_destination'] = "vdk_heartbeat_destination_vdk_heartbeat_1179097076"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_vdk_heartbeat_1179097076"
+    props['job_name'] = "vdk-heartbeat-release-test-job-1708080989"
+    props['execute_template'] = "True"
+    props['ingest_target'] = "vdk-heartbeat-datasource"
+    props['ingest_method'] = "http"
+    props['ingest_destination_table'] = "vdk_heartbeat_ingestion_test"
+    props['ingest_timeout'] = "300"
+    job_input.set_all_properties(props)
+        
