73d357ee137fa9b930262a40c26880f7220ac9e9
aivanov <>
2024-03-14 05:39:32 +0000

    Update data job: quickstart-vdk-release-test-job1710394764
    Updating heartbeat data job
    
    Signed-off-by: aivanov

---
+++ quickstart-vdk-release-test-job1710394764/06_override_properties.py
@@ -0,0 +1,15 @@
+
+def run(job_input):
+    props = job_input.get_all_properties()
+    props['db'] = "taurus_testing_sandbox"
+    props['table_source'] = "vdk_heartbeat_source_root_1710394764"
+    props['table_destination'] = "vdk_heartbeat_destination_root_1710394764"
+    props['table_load_destination'] = "vdk_heartbeat_load_destination_root_1710394764"
+    props['job_name'] = "quickstart-vdk-release-test-job1710394764"
+    props['execute_template'] = "True"
+    props['ingest_target'] = "vdk-heartbeat-datasource"
+    props['ingest_method'] = "http"
+    props['ingest_destination_table'] = "vdk_heartbeat_ingestion_test"
+    props['ingest_timeout'] = "300"
+    job_input.set_all_properties(props)
+        
